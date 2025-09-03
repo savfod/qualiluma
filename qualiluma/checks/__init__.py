ca13c583661cf6f1bf098d322fdbf1d3c693e2ec
Alex Savchik <savsmail@gmail.com>
2025-09-03 15:41:22 +0200

    based -> simple

--- qualiluma/checks/__init__.py
+++ qualiluma/checks/__init__.py
@@ -12,7 +12,7 @@ from .base import (
 )
 from .case_consistency import CaseConsistencyChecker
 from .endsline import check_trailing_newline, should_check_file
-from .llm_based_checker import LLMSimpleChecker
+from .llm_simple_checker import LLMSimpleChecker
 from .variable_consistency import VariablesConsistencyChecker
 
 __all__ = [
